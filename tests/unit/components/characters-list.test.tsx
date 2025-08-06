590eb87888f5a70dae833cabcae40e63182c0e27
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2025-08-06 20:01:33 -0300

    feat(components): add characters list

---
+++ tests/unit/components/characters-list.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+import { CharactersList } from "~/components/characters-list";
+import { results } from "~/data.json";
+
+test("Characters list should contain a list of characters", () => {
+  const characters = results.splice(0, 3);
+
+  render(<CharactersList characters={characters} />);
+
+  expect(screen.getAllByRole("img")).toHaveLength(characters.length);
+
+  characters.forEach((character) => {
+    expect(screen.getByText(character.name)).toBeVisible();
+  });
+});
+
+test("Characters list should be empty", () => {
+  render(<CharactersList characters={[]} />);
+  expect(screen.queryAllByRole("img")).toHaveLength(0);
+});
