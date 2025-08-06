e2eca95803d820ec5ce3e540dcc95f12908cfa18
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2025-08-06 19:06:21 -0300

    feat(components): add character card

---
+++ src/components/character-card.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import type { Character } from "~/types/response";
+
+type Props = {
+  character: Character;
+};
+
+export function CharacterCard({ character }: Props) {
+  return (
+    <article
+      key={character.id}
+      className="relative rounded-md hover:shadow-2xl transition-shadow"
+    >
+      <Image
+        src={character.image}
+        width={320}
+        height={460}
+        alt={character.name}
+        className="w-full object-cover object-center rounded-md rounded-b-none"
+      />
+      <div className="px-4 py-6 border-2 border-t-0 rounded-b-md h-36">
+        <h3 className="font-bold">{character.name}</h3>
+        <h4 className="text-sm font-semibold">Status: {character.status}</h4>
+        <h5 className="text-sm font-medium">
+          Location: {character.location.name}
+        </h5>
+
+        <Link href={`/${character.id}`} className="absolute inset-0">
+          <span className="sr-only">
+            Ver detalhes do personagem {character.name}
+          </span>
+        </Link>
+      </div>
+    </article>
+  );
+}
---
+++ tests/unit/components/character-card.test.tsx
@@ -0,0 +1,21 @@
+import { results } from "~/data.json";
+import { CharacterCard } from "~/components/character-card";
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+
+test("Character information should be visible in character card", () => {
+  const character = results[0];
+
+  render(<CharacterCard character={character} />);
+
+  expect(screen.getByAltText(character.name)).toBeVisible();
+  expect(screen.getByText(character.name)).toBeVisible();
+
+  expect(screen.getByRole("heading", { level: 5 })).toHaveTextContent(
+    `Location: ${character.location.name}`,
+  );
+
+  expect(screen.getByRole("heading", { level: 4 })).toHaveTextContent(
+    `Status: ${character.status}`,
+  );
+});
